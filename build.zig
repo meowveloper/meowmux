d69b277afcc2c8bde1c5c1c5944280d849784895
meowveloper <meowveloper.dev@gmail.com>
2026-01-15 15:33:29 +0700

    build.zig

--- build.zig
+++ build.zig
@@ -4,13 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe = b.addExecutable(.{
         .name = "meowmux",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = exe_mod
     });
 
     b.installArtifact(exe);
@@ -24,7 +26,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
@@ -33,5 +34,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
-
 }
